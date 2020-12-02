d7d7d3497429d6ff822e815a03a16d593149e303
aj96818 <44619176+aj96818@users.noreply.github.com>
2020-12-02 10:29:53 -0500

    Update HGMX_WRAPPER.sql

--- HGMX_WRAPPER.sql
+++ HGMX_WRAPPER.sql
@@ -1,47 +1,49 @@
 USE [GTStage_Matt]
 GO
-/****** Object:  StoredProcedure [dbo].[HGMX_WRAPPER]    Script Date: 12/2/2020 10:28:02 AM ******/
+/****** Object:  StoredProcedure [dbo].[HGMX_WRAPPER_EXCEPTION]    Script Date: 12/2/2020 10:29:16 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
--- =============================================
--- Author: ERIC SOLOMON / Alan Jackson
-/* Description: WRAPPER SPROC FOR HGMX CASH SPS
-	This stored procedure sets the run date for the individual cash revenue SPROCs
-	and runs them - halting the creation of the journal entries if exceptions are encountered
-	while running any of the individual SPROCs.
-	
-	The individual SPROCs that this Wrapper SPROC executes are:
-		HGMX_PayU_Sales
-		HGMX_DLOCAL_CASH 
-		HGMX_DLOCAL_SALES 
-		HGMX_PAYPAL_SALES
 
-	The conditions that stop the completion of the Wrapper SPROC are the following:
+/* Description: WRAPPER EXCEPTION SPROC FOR HGMX CASH SPS
+	This stored procedure is run to generate all of the cash
+	journal entries for only the day after the last successfully 
+	run day when a high suspense amount causes 
+	the main Wrapper SPS to error out.  
+
+	The Wrapper Exception SPS will error out if the following conditions are met:
 		1. If any of the Cash Receipt or Sales journal entries are unbalanced by more than
-			$100, the Wrapper SPS terminates. "Unbalanced Entry" email is sent.
-		2. If any of the cash receipt or sales JEs have a total suspense > $2,500, then the Wrapper SPS terminates.
-		   Suspense is the difference between the txn amount in the merchant report versus the GT report.
-		   "HGMX High Exceptions @vDate" email is sent.
-		3. If the Wrapper fails to execute any of the individual Cash stored procedures for any reason other than the ones listed above,
-			the Wrapper terminates.  "DB - HGMX PROCESSING ERROR" email is sent. */
--- =============================================
+			$100, the Wrapper Exception SPS terminates and an "Unbalanced Entry" email is sent.
+		2. If the Wrapper Exception fails to execute any of the individual Cash stored procedures for any reason other than 
+			an unbalanced entry and high suspense amount it will terminate and a "DB - HGMX PROCESSING ERROR" email is sent.
+	
+	The individual Cash SPROCs that the Wrapper Exeption SPROC executes are:
 
-ALTER   PROCEDURE [dbo].[HGMX_WRAPPER] AS
+		EXEC HGMX_DLOCAL_SALES @vGT_DATE;
+		EXEC HGMX_DLOCAL_CASH @vGT_DATE;
+		EXEC HGMX_PayU_Sales @vGT_DATE;
+		EXEC HGMX_PAYPAL_SALES @vGT_DATE;	
+		EXEC HGMX_OVERPAYMENT_SALES @vGT_DATE;
+
+	  */
+
+
+ALTER     PROCEDURE [dbo].[HGMX_WRAPPER_EXCEPTION] AS
 
 SET NOCOUNT ON;
 
--- Declaring variables for e-mail subject and body to use in Exceptions e-mails.
+--Declaring variables for e-mail subject and body to use in Exceptions e-mails.
+
 DECLARE @vSUBJECT VARCHAR(255)
 DECLARE @VBODY VARCHAR(255)
 DECLARE @PROC_DATE DATETIME
---Declare @filenames varchar(max)
 
 
 /* This sets the run date for the Wrapper SPS to a day after the 
 last day that the Wrapper SPS ran successfully given by the JE_RUN_HISTORY table */
+
 SET @PROC_DATE = (
 		SELECT DATEADD(DD, 1, MAX(DATE_RUN))
 		FROM JE_RUN_HISTORY
@@ -62,13 +64,9 @@ SET @END_DATE = (
 		
 DECLARE @vGT_DATE VARCHAR(50);
 
-/* Beginning of the loop that runs the individual stored procedures
-starting at the last successful run date and incrementing a day at a time
-until 'procedure date' is less than or equal to 'end date.'  */
-
 BEGIN TRY
-	WHILE (@PROC_DATE <= @END_DATE)
-	BEGIN
+--	WHILE (@PROC_DATE <= @END_DATE)
+--	BEGIN
 		SET @vGT_DATE = FORMAT(@PROC_dATE, 'yyyyMMdd')
 		SET @START_DATE = getdate();
 
@@ -79,10 +77,17 @@ BEGIN TRY
 		DELETE
 		FROM GTSTAGE.dbo.COMMON_JE
 		WHERE BATCHID_CALC = @vDEL_DATE
-			AND BRAND_CALC = 'HGMX'
+			AND BRAND_CALC = 'HGMX';
+			SELECT * FROM GTSTAGE.dbo.COMMON_JE
+		DELETE
+		FROM GTSTAGE.dbo.COMMON_EXCEPTION
+		WHERE BATCHID_calc = @vDEL_DATE
+			AND BRAND_calc = 'HGMX';
 
-		/* Calling the four stored procedures that
-		 build a day's worth of Journal Entries for Rev Accounting. */
+--Run Stored Procedures
+
+		/* Executing the five HGMX Cash SPS which we use to build
+		   a day's worth of Journal Entries. */ 
 
 		EXEC HGMX_DLOCAL_SALES @vGT_DATE;
 		EXEC HGMX_DLOCAL_CASH @vGT_DATE;
@@ -90,12 +95,12 @@ BEGIN TRY
 		EXEC HGMX_PAYPAL_SALES @vGT_DATE;	
 		EXEC HGMX_OVERPAYMENT_SALES @vGT_DATE;
 
-		--BALANCE TESTS
-				/*The subsequent code blocks check to see if each final JE table is balanced or not.
-				If the difference between the sum and the credits is greater than $100 than 
-				the Wrapper stops here and an Unbalanced Exceptions email is sent out. */
-		
--- HGMX_PAYU_SALES
+--BALANCE TESTS
+		--HGMX PayU Sales JE
+					/*The HGMX Litle Sales JE is checked whether it is balanced or not.
+					An Unbalanced Exceptions email is sent out if the difference between sum 
+					and the credits is greater than $100 and the wrapper would stop there. */
+	
 		IF ABS((
 					SELECT (SUM(DEBIT) - SUM(credit))
 					FROM HGMX_PAYU_JE_Final
@@ -103,8 +108,8 @@ BEGIN TRY
 					)) > 100
 			--SEND EMAIL ABOUT UNBALANCE GREATER THAN 100 AND STOP JOB
 		BEGIN
-			SET @vSUBJECT = 'DB - HGMX Unbalanced Entries "HGMX_PayU_JE_Final" ' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_PayU_JE_Final" ' + @vGT_DATE + '. Please check tables.';
+			SET @vSUBJECT = 'DB - HGMX  Unbalanced Entries "HGMX_PAYU_JE_Final" ' + @vGT_DATE;
+			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_PAYU_JE_Final" ' + @vGT_DATE + ' are now available.';
 
 			EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com;prakasha.b@endurance.com'
 				,@subject = @vSUBJECT
@@ -118,10 +123,13 @@ BEGIN TRY
 					GROUP BY CHECKBOOKID
 					)) > 0
 			--RUN BALANCE ENTRY
-			EXECUTE dbo.sp_balance_entry_cp_NO_CR 'HGMX_PayU_JE_Final'
+			EXECUTE dbo.sp_balance_entry_cp_NO_CR 'HGMX_PAYU_JE_Final'
 				,'061-11045-000';
 
---HGMX_DLOCAL_Cash
+--DLOCAL CASH JE
+				/*This code block checks to see if the HGMX DLOCAL CASH JE is balanced or not.
+				If the difference between the sum and the credits is greater than $100 than 
+				the Wrapper stops here and an Unbalanced Exceptions email is sent out. */
 		IF ABS((
 					SELECT (SUM(DEBIT) - SUM(credit))
 					FROM HGMX_DLocal_Cash_JE
@@ -130,7 +138,7 @@ BEGIN TRY
 			--SEND EMAIL ABOUT UNBALANCE GREATER THAN 100 AND STOP JOB
 		BEGIN
 			SET @vSUBJECT = 'DB - HGMX Unbalanced Entries "HGMX_DLocal_Cash_JE" ' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_DLocal_Cash_JE" ' + @vGT_DATE + '. Please check tables.';
+			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_DLocal_Cash_JE" ' + @vGT_DATE + ' are now available.';
 
 			EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com;prakasha.b@endurance.com'
 				,@subject = @vSUBJECT
@@ -147,7 +155,11 @@ BEGIN TRY
 			EXECUTE dbo.sp_balance_entry_cp_NO_CR 'HGMX_DLocal_Cash_JE'
 				,'061-11045-000';
 
---HGMX_DLOCAL_Sales
+
+--HGMX_DLOCAL_SALES JE
+			/*This code block checks to see if the HGMX DLOCAL Sales JE is balanced or not.
+				If the difference between the sum and the credits is greater than $100 than 
+				the Wrapper stops here and an Unbalanced Exceptions email is sent out. */
 		IF ABS((
 					SELECT (SUM(DEBIT) - SUM(credit))
 					FROM HGMX_DLOCAL_SALES_JE_FINAL
@@ -155,8 +167,8 @@ BEGIN TRY
 					)) > 100
 			--SEND EMAIL ABOUT UNBALANCE GREATER THAN 100 AND STOP JOB
 		BEGIN
-			SET @vSUBJECT = 'DB - HGMX  Unbalanced Entries "HGMX_DLOCAL_SALES_JE_FINAL" ' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_DLOCAL_SALES_JE_FINAL" ' + @vGT_DATE + '. Please check tables.';
+			SET @vSUBJECT = 'DB - HGMX Unbalanced Entries "HGMX_DLOCAL_SALES_JE_FINAL" ' + @vGT_DATE;
+			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_DLOCAL_SALES_JE_FINAL" ' + @vGT_DATE + ' are now available.';
 
 			EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com;prakasha.b@endurance.com'
 				,@subject = @vSUBJECT
@@ -173,16 +185,19 @@ BEGIN TRY
 			EXECUTE dbo.sp_balance_entry_cp_NO_CR 'HGMX_DLOCAL_SALES_JE_FINAL'
 				,'061-11045-000';
 
---HGMX_PAYPAL_SALES
+--HGMX_PAYPAL_SALES JE
+			/*This code block checks to see if the HGMX PAYPAL SALES JE is balanced or not.
+				If the difference between the sum and the credits is greater than $100 than 
+				the Wrapper stops here and an Unbalanced Exceptions email is sent out. */
 		IF ABS((
 					SELECT (SUM(DEBIT) - SUM(credit))
-					FROM HGMX_PP_JE_Final
+					FROM HGMX_PP_JE_FINAL
 					GROUP BY CHECKBOOKID
 					)) > 100
 			--SEND EMAIL ABOUT UNBALANCE GREATER THAN 100 AND STOP JOB
 		BEGIN
 			SET @vSUBJECT = 'DB - HGMX Unbalanced Entries "HGMX_PP_JE_Final" ' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_PP_JE_Final" ' + @vGT_DATE + '. Please check tables.';
+			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_PP_JE_Final" ' + @vGT_DATE + ' are now available.';
 
 			EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com;prakasha.b@endurance.com'
 				,@subject = @vSUBJECT
@@ -199,16 +214,19 @@ BEGIN TRY
 			EXECUTE dbo.sp_balance_entry_cp_NO_CR 'HGMX_PP_JE_Final'
 				,'061-11045-000';
 
--- HGMX_OVERPAYMENT_SALES
+--HGMX OVERPAYMENT SALES JE
+			/*This code block checks to see if the HGMX OVERPAYMENT SALES JE is balanced or not.
+				If the difference between the sum and the credits is greater than $100 than 
+				the Wrapper stops here and an Unbalanced Exceptions email is sent out. */
 		IF ABS((
-					SELECT (SUM(DEBIT) - SUM(CREDIT))
+					SELECT (SUM(DEBIT) - SUM(credit))
 					FROM HGMX_OVERPAYMENT_JE
 					GROUP BY CHECKBOOKID
-					)) > 50
-		--SEND EMAIL ABOUT UNBALANCE GREATER THAN 50 AND STOP JOB
+					)) > 100
+			--SEND EMAIL ABOUT UNBALANCE GREATER THAN 100 AND STOP JOB
 		BEGIN
 			SET @vSUBJECT = 'DB - HGMX Unbalanced Entries "HGMX_OVERPAYMENT_JE" ' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_OVERPAYMENT_JE" ' + @vGT_DATE + '. Please check tables.';
+			SET @vBODY = 'DB - HGMX Unbalanced Entries for "HGMX_OVERPAYMENT_JE" ' + @vGT_DATE + ' are now available.';
 
 			EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com;prakasha.b@endurance.com'
 				,@subject = @vSUBJECT
@@ -222,64 +240,72 @@ BEGIN TRY
 					GROUP BY CHECKBOOKID
 					)) > 0
 			--RUN BALANCE ENTRY
-			EXECUTE dbo.sp_balance_entry_cp_NO_CR 'HGMX_OVERPAYMENT_JE'
-				,'061-11045-000';
+			EXECUTE dbo.sp_balance_entry_cp 'HGMX_OVERPAYMENT_JE'
+				,'061-11045-000';		
+
+--EXCEPTION TESTS
+--IF ABS((
+--					SELECT SUM(UN.DEBIT) - SUM(UN.CREDIT)
+--					FROM (
+--						SELECT credit
+--							,debit
+--							,account
+--						FROM HGMX_Litle_Final_JE
+						
+--						UNION
+						
+--						SELECT credit
+--							,debit
+--							,account
+--						FROM HGMX_AMEX_Final_JE
+						
+--						UNION
+						
+--						SELECT credit
+--							,debit
+--							,account
+--						FROM HGMX_AMEX_Cash_JE_Final 
+						
+--						UNION
+						
+--						SELECT credit
+--							,debit
+--							,account
+--						FROM HGMX_Litle_Cash_JE_Final
+						
+--						UNION
+						
+--						SELECT credit
+--							,debit
+--							,account
+--						FROM HGMX_PP_JE_Final
+						
+--						UNION
+						
+--						SELECT credit
+--							,debit
+--							,account
+--						FROM HGMX_Checks_JE
+						
+						
+--						) UN
+--					WHERE UN.ACCOUNT IN ('073-11045-000')
+--						--GROUP BY UN.ACCOUNT
+--					)) > 5000
+--		BEGIN
+--			--SEND EMAIL ABOUT HIGH SUSPENSE AND STOP JOB
+--			SET @vSUBJECT = 'DB - HGMX High Exceptions ' + @vGT_DATE;
+--			SET @vBODY = 'DB - HGMX High Exceptions ' + @vGT_DATE + ' are now available.';
+
+--			EXEC msdb.dbo.sp_send_dbmail @recipients = 'acl_reporting@endurance.com;prakasha.b@endurance.com'
+--				,@subject = @vSUBJECT
+--				,@body = @vBODY
+
+--			RETURN;
+--		END
 
 
-										
--- Next step in Wrapper: Check for X's in the respective "Account" fields for each final JE and stop the Wrapper if any are present.
-		IF (SELECT COUNT(*) FROM
-					(SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_PAYU_JE_Final
-					UNION 
-					SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_DLOCAL_SALES_JE_FINAL
-					UNION
-					SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_PP_JE_Final
-					UNION
-					SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_DLocal_Cash_JE
-					UNION
-					SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_OVERPAYMENT_JE
-					) UN
-				WHERE UN.ACCOUNT LIKE ('%XX%')) > 0
-		BEGIN
-		-- Execute block of code if X's in Account
-			SET @vSUBJECT = 'DB - HGMX Xs in Account Numbers' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX Xs in Account Numbers' + @vGT_DATE + '. Please check tables.';
-
-			EXEC msdb.dbo.sp_send_dbmail @recipients = 'acl_reporting@endurance.com;prakasha.b@endurance.com'
-				,@subject = @vSUBJECT
-				,@body = @vBODY
-			RETURN;
-		END
-		
--- EXCEPTION TESTS
-
-IF ABS((
-		SELECT (SUM(UN.DEBIT) - SUM(UN.CREDIT))
-			FROM (SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_PAYU_JE_Final
-				UNION
-	     		SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_PP_JE_Final
-				UNION
-				SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_DLOCAL_SALES_JE_FINAL
-				UNION
-				SELECT CREDIT, DEBIT, ACCOUNT FROM HGMX_DLocal_Cash_JE
-				) UN
-		WHERE UN.ACCOUNT IN ('061-11045-000')
-		)) > 2500
-
-		BEGIN
-			--SEND EMAIL ABOUT HIGH SUSPENSE AND STOP JOB
-			SET @vSUBJECT = 'DB - HGMX High Exceptions ' + @vGT_DATE;
-			SET @vBODY = 'DB - HGMX High Exceptions ' + @vGT_DATE + '. Please check tables.';
-
-			EXEC msdb.dbo.sp_send_dbmail @recipients = 'acl_reporting@endurance.com;prakasha.b@endurance.com'
-				,@subject = @vSUBJECT
-				,@body = @vBODY
-
-			RETURN;
-		END
-
-
--- COMMON JE INSERTS
+		-- COMMON JE INSERTS
 
 ---- HGMX_PAYU
 INSERT INTO COMMON_JE (CHECKBOOKID_CALC, BATCHID_CALC, TRANTYPE_CALC, TRANDATE_CALC, SRCDOC_CALC, CURRID_CALC, REFRENCE_CALC, ACCOUNT_CALC, DEBIT, CREDIT, DISTREF_CALC, KEY1_CALC, REVERSEDATE_CALC, UNIQUEID_CALC, DOCAMT_CALC, DISTYPE_CALC, BRAND_CALC, MERCHANT_CALC)
@@ -308,17 +334,18 @@ INSERT INTO COMMON_JE (CHECKBOOKID_CALC, BATCHID_CALC, TRANTYPE_CALC, TRANDATE_C
 SELECT CHECKBOOKID, BATCHID, TRANTYPE, TRANDATE, SRCDOC, CURRID, REFERENCE, ACCOUNT, DEBIT, CREDIT, DISTREF, KEY1, REVERSEDATE, UNIQUEID, DOCAMT, DISTYPE, 'HGMX', 'Overpayment_JE'
 FROM HGMX_OVERPAYMENT_JE
 
--- COMMON EXCEPTION INSERTS
 
-		-- CREATE EXTRACTS HERE
+		--COMMON EXCEPTION INSERTS
 
-		--EXECUTE [DMFLogging].[dbo].[sp_HGMX_JE_Feed] @vGT_DATE;
 
-		/* If Wrapper SPS runs up to this point, insert the date
-		that got run (along with some add'l details) into the 
-		JE_RUN_HISTORY table so that the @PROC_DATE (procedure date)
-		gets incremented by one day and continues the WHILE loop
-		until @PROC_DATE <= @END_DATE */
+		--####################
+		--     CREATE EXTRACTS HERE
+		--####################
+		EXECUTE [DMFLogging].[dbo].[sp_HGMX_JE_Feed] @vGT_DATE;
+
+		/* If Wrapper Exception SPS runs up to this point, insert the run date 
+		into the following tables: JE_RUN_HISTORY & EXCEPTION_RUN_HISTORY
+		so that the following day can get run the next time the Wrapper is executed. */
 
 		INSERT INTO JE_RUN_HISTORY (
 			run_id
@@ -334,7 +361,23 @@ FROM HGMX_OVERPAYMENT_JE
 			,'GT_PROCESSED_HGMX'
 		FROM JE_RUN_HISTORY
 
+		INSERT INTO EXCEPTION_RUN_HISTORY (
+			run_id
+			,start_date
+			,stop_Date
+			,date_run
+			,brand_table
+			, RUN_TYPE
+			)
+		SELECT max(run_id) + 1
+			,@START_DATE
+			,getdate()
+			,@PROC_DATE
+			,'GT_PROCESSED_HGMX'
+			,'EXCEPTION'
+		FROM EXCEPTION_RUN_HISTORY;
 
+		
 		SET @vSUBJECT = 'DB - HGMX Journal Entries and Exception Reports ' + @vGT_DATE;
 		SET @vBODY = 'DB - Journal Entries and Exception Reports for ' + @vGT_DATE + ' are now available.';
 
@@ -342,28 +385,11 @@ FROM HGMX_OVERPAYMENT_JE
 			,@subject = @vSUBJECT
 			,@body = @vBODY;
 
-
-		--Set @filenames = '\\corp.endurance.com\acl\axcore\Fin Ops Job Files\Entries\Sale\'+ @vGT_DATE + '_PAYU_JE_HGMX.xlsx'
-
-		--Set @filenames = (CASE WHEN (SELECT COUNT(*) FROM HGMX_PayU_Cash_JE_Final) > 0 THEN @filenames + ';\\corp.endurance.com\acl\axcore\Fin Ops Job Files\Entries\Cash_Rec\'+ @vGT_DATE + '_PayUCash_JE_HGMX.xlsx'
-		--ELSE @filenames END)
-
-		--Set @filenames = (CASE WHEN (SELECT COUNT(*) FROM HGMX_PayU_Exception_Report) > 0 THEN @filenames + ';\\corp.endurance.com\acl\axcore\Fin Ops Job Files\Entries\Exception_Reports\'+ @vGT_DATE + '_HGMX_PAYU_Exceptions.xlsx'
-		--ELSE @filenames END)
-		
-		--SET @vSUBJECT = 'DB - HGMX Journal Entries and Exception Reports ' + @vGT_DATE;
-		--SET @vBODY = 'DB - Journal Entries and Exception Reports for ' + @vGT_DATE + ' are now available.';
-
-		--EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com'--,apac-obaccounts@endurance.com'
-		--	,@subject = @vSUBJECT
-		--	,@body = @vBODY
-		--	,@file_attachments = @filenames;
-
-		EXEC HGMX_DROPS;
+		EXEC HGMX_DROPS
 
 		SET @PROC_DATE = DATEADD(DD, 1, @PROC_DATE)
 			
-	END
+--	END
 END TRY
 
 BEGIN CATCH
@@ -371,7 +397,7 @@ BEGIN CATCH
 	SET @vSUBJECT = 'DB - HGMX PROCESSING ERROR ' + @vGT_DATE;
 	SET @vBODY = 'DB - PROCESSING ERROR CHECK FILES ' + @vGT_DATE;
 
-	EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com,prakasha.b@endurance.com'
+	EXEC msdb.dbo.sp_send_dbmail @recipients = 'ACL_REPORTING@endurance.com'
 		,@subject = @vSUBJECT
 		,@body = @vBODY;
 
