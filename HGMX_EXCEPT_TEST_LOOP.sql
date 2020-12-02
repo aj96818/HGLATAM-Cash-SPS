7ffe6d01a9c7f2b37c58a1076761462e8abaa6b9
aj96818 <44619176+aj96818@users.noreply.github.com>
2020-12-02 10:23:56 -0500

    Update HGMX_EXCEPT_TEST_LOOP.sql
    Final version - used for Nov'20 cash close.

--- HGMX_EXCEPT_TEST_LOOP.sql
+++ HGMX_EXCEPT_TEST_LOOP.sql
@@ -1,6 +1,6 @@
 USE [GTStage_Matt]
 GO
-/****** Object:  StoredProcedure [dbo].[HGMX_EXCEPT_TEST_LOOP]    Script Date: 11/13/2020 3:49:22 PM ******/
+/****** Object:  StoredProcedure [dbo].[HGMX_EXCEPT_TEST_LOOP]    Script Date: 12/2/2020 10:23:21 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -11,8 +11,8 @@ DECLARE @PROC_DATE DATETIME
 DECLARE @FINISH_DATE DATETIME
 DECLARE @vGT_DATE VARCHAR(50);
 
---SET @PROC_DATE = '20201001'
---SET @FINISH_DATE = '20201005'
+--SET @PROC_DATE = '20201101'
+--SET @FINISH_DATE = '20201130'
 SET @vGT_DATE = FORMAT(@PROC_dATE, 'yyyyMMdd')
 
 --DROP TABLE HGMX_GT_ALL
@@ -23,7 +23,10 @@ SET @vGT_DATE = FORMAT(@PROC_dATE, 'yyyyMMdd')
 --DROP TABLE HGMX_PAYU_Exception_Report_ALL
 --DROP TABLE HGMX_GT_DLOCAL_FullJoin_All
 
---CREATE TABLE HGMX_GT_ALL (Brand VARCHAR(75), ClientID VARCHAR(55), Processor VARCHAR(25), Unique_Trans_ID VARCHAR(155), Bill_Group VARCHAR(25), Bill_Type VARCHAR(25), Date DATE, Time Time, Term VARCHAR(25), Term_Start_Date Date, Amount DECIMAL(15,2), Currency VARCHAR(20), Taxes DECIMAL(15,2), Payment_Amount DECIMAL(15,2), Payment_Currency VARCHAR(20), Offer_ID VARCHAR(125), Offer_Name VARCHAR(225), Grouping VARCHAR(55), Rebill_or_New VARCHAR(50), Country VARCHAR(20), Document VARCHAR(88), Transaction_Type VARCHAR(55), Invoice_ID VARCHAR(55), Invoice_Item_ID VARCHAR(55), Quantity VARCHAR(55), Product_Id VARCHAR(25), Full_Item_Description VARCHAR(255), Product_Date_Register Date, Product_Lifetime DECIMAL(10,2), Service_Period VARCHAR(200), PromoCode VARCHAR(155), PromoCode_Type VARCHAR(145), PromoCode_Value VARCHAR(55), Late_Fee VARCHAR(55), VAT_Tax VARCHAR(55), Net_Value DECIMAL(15,2), Net_Value_After_Taxes DECIMAL(15,2), Total_Credit_Applied VARCHAR(55), Total_Invoice DECIMAL(15,2), Total_Transaction DECIMAL(15,2), Merchant_Rates VARCHAR(55), Current_Invoice_Status VARCHAR(45), Current_Product_Status VARCHAR(50), Invoice_Date_Paid Date, TRANS_AMOUNT_USD_CALC DECIMAL(15,2), TRANS_AMOUNT_LOCAL_CALC DECIMAL(15,2), TAX_AMOUNT_LOCAL_CALC DECIMAL(15,2), TAX_AMOUNT_USD_CALC DECIMAL(15,2), REVENUE_AMOUNT_LOCAL_CALC DECIMAL(15,2), REVENUE_AMOUNT_USD_CALC DECIMAL(15,2), Business_Line_CALC VARCHAR(55), Type_CALC VARCHAR(25), SettledBy_CALC VARCHAR(50), DLOCAL_TXN_ID VARCHAR(75))
+--SELECT * FROM HGMX_GT
+--CREATE TABLE HGMX_GT_ALL (Brand VARCHAR(75), ClientID VARCHAR(55), Processor VARCHAR(25), Unique_Trans_ID VARCHAR(155), Bill_Group VARCHAR(25), Bill_Type VARCHAR(25), Date DATE, Time Time, Term VARCHAR(25), Term_Start_Date Date, Amount DECIMAL(15,2), Currency VARCHAR(20), Taxes DECIMAL(15,2), Payment_Amount DECIMAL(15,2), Payment_Currency VARCHAR(20), Offer_ID VARCHAR(125), Offer_Name VARCHAR(225), Grouping VARCHAR(55), Rebill_or_New VARCHAR(50), Country VARCHAR(20), Document VARCHAR(88), Transaction_Type VARCHAR(55), Invoice_ID VARCHAR(55), Invoice_Item_ID VARCHAR(55), Quantity VARCHAR(55), Product_Id VARCHAR(25), Full_Item_Description VARCHAR(255), Product_Date_Register Date, Product_Lifetime DECIMAL(10,2), Service_Period VARCHAR(200), PromoCode VARCHAR(155), PromoCode_Type VARCHAR(145), PromoCode_Value VARCHAR(55), Late_Fee VARCHAR(55), VAT_Tax VARCHAR(55), Net_Value DECIMAL(15,2), Net_Value_After_Taxes DECIMAL(15,2), Total_Credit_Applied VARCHAR(55), Total_Invoice DECIMAL(15,2), Total_Transaction DECIMAL(15,2), Merchant_Rates VARCHAR(55), Current_Invoice_Status VARCHAR(45), Current_Product_Status VARCHAR(50), Invoice_Date_Paid Date, TRANS_AMOUNT_USD_CALC DECIMAL(15,2), TRANS_AMOUNT_LOCAL_CALC DECIMAL(15,2), TAX_AMOUNT_LOCAL_CALC DECIMAL(15,2), TAX_AMOUNT_USD_CALC DECIMAL(15,2), REVENUE_AMOUNT_LOCAL_CALC DECIMAL(15,2), REVENUE_AMOUNT_USD_CALC DECIMAL(15,2), Business_Line_CALC VARCHAR(55), Type_CALC VARCHAR(25), SettledBy_CALC VARCHAR(50), DLOCAL_TXN_ID VARCHAR(75), Overpayment_Flag VARCHAR(5), Prepaid_Deposit_Flag VARCHAR(5), CASH_INCL_CALC INT, GAAP_INCL_CALC INT)
+
+
 --CREATE TABLE HGMX_DLOCAL_SALES_JE_ALL (CHECKBOOKID VARCHAR(75), BATCHID VARCHAR(50), TRANDATE VARCHAR(50), REFERENCE VARCHAR(255), ACCOUNT VARCHAR(75), DISTREF VARCHAR(255), DEBIT DECIMAL(13,2), CREDIT DECIMAL(13,2), UNIQUEID VARCHAR(50), DISTYPE INT, DOCAMT DECIMAL(15,2))
 --CREATE TABLE HGMX_PAYPAL_SALES_JE_ALL (CHECKBOOKID VARCHAR(75), BATCHID VARCHAR(50), TRANDATE VARCHAR(50), REFERENCE VARCHAR(255), ACCOUNT VARCHAR(75), DISTREF VARCHAR(255), DEBIT DECIMAL(13,2), CREDIT DECIMAL(13,2), UNIQUEID VARCHAR(50), CASHRCPT VARCHAR(55), DISTYPE VARCHAR(25), DOCAMT DECIMAL(14,2))
 --CREATE TABLE HGMX_PAYU_SALES_JE_ALL (CHECKBOOKID VARCHAR(75), BATCHID VARCHAR(50), TRANDATE VARCHAR(50), REFERENCE VARCHAR(255), ACCOUNT VARCHAR(75), DISTREF VARCHAR(255), DEBIT DECIMAL(13,2), CREDIT DECIMAL(13,2), UNIQUEID VARCHAR(50), CASHRCPT VARCHAR(55), DISTYPE VARCHAR(25), DOCAMT DECIMAL(14,2))
@@ -62,7 +65,7 @@ EXEC HGMX_DLOCAL_CASH @vGT_DATE
 -- GT Report
 
 INSERT INTO HGMX_GT_ALL
-SELECT  Brand, ClientID, Processor, Unique_Trans_ID, Bill_Group, Bill_Type, Date, Time, Term, Term_Start_Date, Amount, Currency, Taxes, Payment_Amount, Payment_Currency, Offer_ID, Offer_Name, Grouping, Rebill_or_New, Country, Document, Transaction_Type, Invoice_ID, Invoice_Item_ID, Quantity, Product_Id, Full_Item_Description, Product_Date_Register, Product_Lifetime, Service_Period, PromoCode, PromoCode_Type, PromoCode_Value, Late_Fee, VAT_Tax, Net_Value, Net_Value_After_Taxes, Total_Credit_Applied, Total_Invoice, Total_Transaction, Merchant_Rates, Current_Invoice_Status, Current_Product_Status, Invoice_Date_Paid, TRANS_AMOUNT_USD_CALC, TRANS_AMOUNT_LOCAL_CALC, TAX_AMOUNT_LOCAL_CALC, TAX_AMOUNT_USD_CALC, REVENUE_AMOUNT_LOCAL_CALC, REVENUE_AMOUNT_USD_CALC, Business_Line_CALC, Type_CALC, SettledBy_CALC, DLOCAL_TXN_ID FROM HGMX_GT
+SELECT  Brand, ClientID, Processor, Unique_Trans_ID, Bill_Group, Bill_Type, Date, Time, Term, Term_Start_Date, Amount, Currency, Taxes, Payment_Amount, Payment_Currency, Offer_ID, Offer_Name, Grouping, Rebill_or_New, Country, Document, Transaction_Type, Invoice_ID, Invoice_Item_ID, Quantity, Product_Id, Full_Item_Description, Product_Date_Register, Product_Lifetime, Service_Period, PromoCode, PromoCode_Type, PromoCode_Value, Late_Fee, VAT_Tax, Net_Value, Net_Value_After_Taxes, Total_Credit_Applied, Total_Invoice, Total_Transaction, Merchant_Rates, Current_Invoice_Status, Current_Product_Status, Invoice_Date_Paid, TRANS_AMOUNT_USD_CALC, TRANS_AMOUNT_LOCAL_CALC, TAX_AMOUNT_LOCAL_CALC, TAX_AMOUNT_USD_CALC, REVENUE_AMOUNT_LOCAL_CALC, REVENUE_AMOUNT_USD_CALC, Business_Line_CALC, Type_CALC, SettledBy_CALC, DLOCAL_TXN_ID, Overpayment_Flag, Prepaid_Deposit_Flag, CASH_INCL_CALC, GAAP_INCL_CALC FROM HGMX_GT
 
 -- Sales Entries
 
@@ -109,51 +112,83 @@ SET @vGT_DATE = FORMAT(@PROC_dATE, 'yyyyMMdd')
 
 END
 
+/*
 
---select * from hgmx_gt_all order by date desc
---select processor, sum(revenue_amount_usd_calc) from hgmx_gt_all where Unique_Trans_ID like '%credit%'
---group by processor
+select * from hgmx_gt_all order by date desc
+select processor, sum(revenue_amount_usd_calc) from hgmx_gt_all where Unique_Trans_ID like '%credit%'
+group by processor
 
----- Total Suspense for all days run by Merchant
---SELECT 'Dlocal' as Merchant, SUM(DEBIT - CREDIT) SUSPENSE FROM HGMX_DLOCAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%'
---UNION
---SELECT 'Paypal' as Merchant, SUM(DEBIT - CREDIT) SUSPENSE FROM HGMX_PAYPAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%'
---UNION
---SELECT 'PayU' as Merchant, SUM(DEBIT - CREDIT) SUSPENSE FROM HGMX_PAYU_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%'
+Total Suspense for all days run by Merchant
 
----- Total suspense by merchant and day.
---WITH dlocal AS (SELECT TRANDATE, SUM(credit - debit) Suspense from HGMX_DLOCAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%' GROUP BY TRANDATE)
---	, paypal AS (SELECT TRANDATE, SUM(credit - debit) Suspense from HGMX_PAYPAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%' GROUP BY TRANDATE)
---	, payu AS (SELECT TRANDATE, SUM(credit - debit) Suspense from HGMX_PAYU_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%' GROUP BY TRANDATE)
---SELECT 
---	dlocal.trandate as TRANDATE
---	, ISNULL(dlocal.Suspense, 0) as [DLOCAL Suspense]
---	, ISNULL(paypal.Suspense, 0) as [Paypal Suspense]
---	, ISNULL(payu.Suspense, 0) as [PayU Suspense]
---FROM
---	dlocal
---LEFT JOIN paypal on dlocal.TRANDATE = paypal.TRANDATE
---LEFT JOIN payu on dlocal.TRANDATE = payu.TRANDATE
---ORDER BY Dlocal.TRANDATE asc
+SELECT 'Dlocal' as Merchant, SUM(DEBIT - CREDIT) SUSPENSE FROM HGMX_DLOCAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%'
+UNION
+SELECT 'Paypal' as Merchant, SUM(DEBIT - CREDIT) SUSPENSE FROM HGMX_PAYPAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%'
+UNION
+SELECT 'PayU' as Merchant, SUM(DEBIT - CREDIT) SUSPENSE FROM HGMX_PAYU_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%'
+
+Total suspense by merchant and day.
+
+WITH dlocal AS (SELECT TRANDATE, SUM(credit - debit) Suspense from HGMX_DLOCAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%' GROUP BY TRANDATE)
+	, paypal AS (SELECT TRANDATE, SUM(credit - debit) Suspense from HGMX_PAYPAL_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%' GROUP BY TRANDATE)
+	, payu AS (SELECT TRANDATE, SUM(credit - debit) Suspense from HGMX_PAYU_SALES_JE_ALL WHERE ACCOUNT LIKE '%11045%' GROUP BY TRANDATE)
+SELECT 
+	dlocal.trandate as TRANDATE
+	, ISNULL(dlocal.Suspense, 0) as [DLOCAL Suspense]
+	, ISNULL(paypal.Suspense, 0) as [Paypal Suspense]
+	, ISNULL(payu.Suspense, 0) as [PayU Suspense]
+FROM
+	dlocal
+LEFT JOIN paypal on dlocal.TRANDATE = paypal.TRANDATE
+LEFT JOIN payu on dlocal.TRANDATE = payu.TRANDATE
+ORDER BY Dlocal.TRANDATE asc
 
 
------- Total Revenue by Merchant for all days run.
---SELECT 'Dlocal' as Merchant, SUM(credit - debit) Revenue from HGMX_DLOCAL_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
---UNION
---SELECT 'Paypal' as Merchant, SUM(credit - debit) Revenue from HGMX_PAYPAL_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
---UNION
---SELECT 'PayU' as Merchant, SUM(credit - debit) Revenue from HGMX_PAYU_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+Total Revenue by Merchant for all days run.
 
---SELECT * FROM HGMX_DLOCAL_SALES_JE_ALL
---SELECT * FROM HGMX_GT_ALL where business_line_calc = 'unknown'
---SELECT * FROM HGMX_PAYPAL_SALES_JE_ALL
---SELECT * FROM HGMX_PAYU_SALES_JE_ALL
---SELECT * FROM HGMX_OVERPAYMENT_JE_ALL
+SELECT 'Dlocal' as Merchant, SUM(credit - debit) Revenue from HGMX_DLOCAL_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+UNION
+SELECT 'Paypal' as Merchant, SUM(credit - debit) Revenue from HGMX_PAYPAL_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+UNION
+SELECT 'PayU' as Merchant, SUM(credit - debit) Revenue from HGMX_PAYU_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+
+
+
+Combined Entries
+
+SELECT * FROM HGMX_GT_ALL
+SELECT * FROM HGMX_PAYPAL_SALES_JE_ALL
+SELECT * FROM HGMX_PAYU_SALES_JE_ALL
+SELECT * FROM HGMX_OVERPAYMENT_JE_ALL
+SELECT * FROM HGMX_DLOCAL_SALES_JE_ALL
+SELECT * FROM HGMX_DLOCAL_CASH_JE_ALL
+
+SELECT * FROM HGMX_DLOCAL_SALES_JE_ALL
+UNION
+SELECT CHECKBOOKID, BATCHID, TRANDATE, REFERENCE, ACCOUNT, DISTREF, DEBIT, CREDIT, UNIQUEID, DISTYPE, DOCAMT FROM HGMX_PAYPAL_SALES_JE_ALL
+UNION
+SELECT CHECKBOOKID, BATCHID, TRANDATE, REFERENCE, ACCOUNT, DISTREF, DEBIT, CREDIT, UNIQUEID, DISTYPE, DOCAMT FROM HGMX_PAYU_SALES_JE_ALL
+
+SELECT (sum(debit)-sum(credit)) diff FROM HGMX_DLOCAL_SALES_JE_ALL
+UNION
+SELECT (sum(debit)-sum(credit)) diff FROM HGMX_PAYPAL_SALES_JE_ALL
+UNION
+SELECT (sum(debit)-sum(credit)) diff FROM HGMX_PAYU_SALES_JE_ALL
+
+
+Revenue entries only:
+
+SELECT * FROM HGMX_DLOCAL_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+UNION
+SELECT CHECKBOOKID, BATCHID, TRANDATE, REFERENCE, ACCOUNT, DISTREF, DEBIT, CREDIT, UNIQUEID, DISTYPE, DOCAMT FROM HGMX_PAYPAL_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+UNION
+SELECT CHECKBOOKID, BATCHID, TRANDATE, REFERENCE, ACCOUNT, DISTREF, DEBIT, CREDIT, UNIQUEID, DISTYPE, DOCAMT FROM HGMX_PAYU_SALES_JE_ALL WHERE SUBSTRING(account, 5, 1) LIKE '%4%'
+
+
 
 
 -- Unbalanced Entries DLOCAL:
 
-/*
+
 SELECT
 TRANDATE, SUM(DEBIT) DEBIT, SUM(CREDIT) CREDIT, (SUM(DEBIT) - SUM(CREDIT)) DIFFERENCE
 FROM HGMX_DLOCAL_SALES_JE_ALL
@@ -166,124 +201,126 @@ FROM GT_Processed_HG_Mexico_New
 WHERE DATE = '2020-10-19'
 GROUP BY TYPE_CALC, BUSINESS_LINE_CALC
 
-*/
+
 -- Exception Reports:
 
---select * from HGMX_DLOCAL_EXCEPTION_REPORT_all
---SELECT * FROM HGMX_Paypal_Exception_Report_ALL
---select * from HGMX_PAYU_Exception_Report_all
+select * from HGMX_DLOCAL_EXCEPTION_REPORT_all
+SELECT * FROM HGMX_Paypal_Exception_Report_ALL
+select * from HGMX_PAYU_Exception_Report_all
 
---SELECT * FROM HGMX_GT_DLOCAL_FULLJOIN_ALL
---SELECT * FROM HGMX_GT_PAYPAL_FULLJOIN_ALL
---SELECT * FROM HGMX_GT_PAYU_FULLJOIN_ALL
+SELECT * FROM HGMX_GT_DLOCAL_FULLJOIN_ALL
+SELECT * FROM HGMX_GT_PAYPAL_FULLJOIN_ALL
+SELECT * FROM HGMX_GT_PAYU_FULLJOIN_ALL
 
 
---select * from GTStage.dbo.GT_Processed_dLocal_all_transactions where transaction_id in ('385949', '386672', '385903', '385919', '385948', '386731')
---and status = 'cleared'
+select * from GTStage.dbo.GT_Processed_dLocal_all_transactions where transaction_id in ('385949', '386672', '385903', '385919', '385948', '386731')
+and status = 'cleared'
 
---select * from GTStage.dbo.gt_processed_hg_mexico_new where invoice_id in ('385949', '386672', '385903', '385919', '385948', '386731')
+select * from GTStage.dbo.gt_processed_hg_mexico_new where invoice_id in ('385949', '386672', '385903', '385919', '385948', '386731')
 
 
 
 ----------------		TOTAL MATCHED & UN-MATCHED TXNS BETWEEN GT & MERCHANT		--------------------------------
 
--- HGMX GT <--> PAYPAL MATCHED & UNMATCHED TXNS
+	HGMX GT <--> PAYPAL MATCHED & UNMATCHED TXNS
 
---with gt as (
---select 
---	isnull(gt_date, pp_activity_date) as gt_Date 
---	,count(isnull(unique_trans_id, 'gt_null')) as GT_NULL_Count 
---	--, sum(zother_diff) as GT_zother_diff
---	--, 'GT' as [Source]
---from hgmx_gt_paypal_fulljoin_all
---where unique_trans_id is null
---group by isnull(gt_date, pp_activity_date)
---),
---pp as (
---select 
---	isnull(pp_activity_date, gt_date) as pp_Date 
---	,count(isnull(pp_txnid_calc, 'pp_null')) as PP_NULL_Count 
---	--, sum(zother_diff) as PP_zother_diff
---	--, 'GT' as [Source]
---from hgmx_gt_paypal_fulljoin_all
---where pp_txnid_calc is null
---group by ISNULL(pp_activity_date, gt_date)
---),
---both as (
---select
---	isnull(gt_date, pp_activity_date) as both_Date
---	, count(*) as Total_Count
-----	, sum(zother_diff) as Both_zother_diff
---from hgmx_gt_paypal_fulljoin_all
---group by isnull(gt_date, pp_activity_date)
---)
-
---select both.both_Date as Date, gt.GT_NULL_Count, pp.PP_NULL_Count, both.Total_Count
---from both
---full join gt on both.both_Date = gt.gt_Date
---full join pp on both.both_Date = pp.pp_Date
---order by both.both_Date 
-
---SELECT * FROM HGMX_GT_DLOCAL_FULLJOIN_ALL
-
-
----- HGMX GT <--> DLOCAL MATCHED & UNMATCHED TXNS
---with gt as (
---select 
---	isnull(gt_date, creation_date) as gt_Date 
---	,count(isnull(invoice_id, 'gt_null')) as GT_NULL_Count 
---	, sum(zother_diff) as GT_zother_diff
---	, 'GT' as [Source]
---from hgmx_gt_dlocal_fulljoin_all
---where invoice_id is null
---group by isnull(gt_date, creation_date)
---),
---dlocal as (
---select 
---	isnull(creation_date, gt_date) as CREATION_DATE
---	,count(isnull(transaction_id, 'dlocal_null')) as DLOCAL_NULL_Count 
---	, sum(zother_diff) as DLOCAL_zother_diff
---	, 'GT' as [Source]
---from hgmx_gt_DLOCAL_fulljoin_all
---where TRANSACTION_ID is null
---group by ISNULL(CREATION_date, gt_date)
---),
---total as (
---select
---	isnull(gt_date, CREATION_date) as total_Date
---	, count(*) as Total_Count
+with gt as (
+select 
+	isnull(gt_date, pp_activity_date) as gt_Date 
+	,count(isnull(unique_trans_id, 'gt_null')) as GT_NULL_Count 
+	--, sum(zother_diff) as GT_zother_diff
+	--, 'GT' as [Source]
+from hgmx_gt_paypal_fulljoin_all
+where unique_trans_id is null
+group by isnull(gt_date, pp_activity_date)
+),
+pp as (
+select 
+	isnull(pp_activity_date, gt_date) as pp_Date 
+	,count(isnull(pp_txnid_calc, 'pp_null')) as PP_NULL_Count 
+	--, sum(zother_diff) as PP_zother_diff
+	--, 'GT' as [Source]
+from hgmx_gt_paypal_fulljoin_all
+where pp_txnid_calc is null
+group by ISNULL(pp_activity_date, gt_date)
+),
+both as (
+select
+	isnull(gt_date, pp_activity_date) as both_Date
+	, count(*) as Total_Count
 --	, sum(zother_diff) as Both_zother_diff
---from hgmx_gt_DLOCAL_fulljoin_all
---group by isnull(gt_date, CREATION_date)
---),
---both as (
---select
---	gt_date as both_date
---	, count(*) as both_count
---from HGMX_GT_DLOCAL_FullJoin_ALL
---where invoice_id is not null and TRANSACTION_ID is not null
---group by GT_DATE
---),
---unmatched as (
---select
---	isnull(gt_date, creation_date) as unmatched_date
---	, count(*) as unmatched_count
---from HGMX_GT_DLOCAL_FullJoin_ALL
---where invoice_id is null or TRANSACTION_ID is null
---group by isnull(gt_date, creation_date) 
---)
+from hgmx_gt_paypal_fulljoin_all
+group by isnull(gt_date, pp_activity_date)
+)
 
---select
---	total.total_Date as Date
---	, gt.GT_NULL_Count as [Not in GT]
---	, DLOCAL.DLOCAL_NULL_Count [Not in DLOCAL]
---	, unmatched.unmatched_count as [Total Un-matched Txns]
---	, both.both_count as [Total Matched Txns]
---	, total.Total_Count [Total Txns]
---	, ((cast(gt.gt_null_count as int) + cast(dlocal.dlocal_null_count as int)) / cast(total.Total_Count as int)) AS [% of Total Txns Un-matched]
---from total
---full join gt on total.Total_Date = gt.gt_Date
---full join DLOCAL on total.Total_Date = DLOCAL.CREATION_Date
---full join both on total.total_Date = both.both_date
---full join unmatched on total.total_Date = unmatched.unmatched_date
---order by total.Total_Date 
+select both.both_Date as Date, gt.GT_NULL_Count, pp.PP_NULL_Count, both.Total_Count
+from both
+full join gt on both.both_Date = gt.gt_Date
+full join pp on both.both_Date = pp.pp_Date
+order by both.both_Date 
+
+SELECT * FROM HGMX_GT_DLOCAL_FULLJOIN_ALL
+
+
+	HGMX GT <--> DLOCAL MATCHED & UNMATCHED TXNS
+with gt as (
+select 
+	isnull(gt_date, creation_date) as gt_Date 
+	,count(isnull(invoice_id, 'gt_null')) as GT_NULL_Count 
+	, sum(zother_diff) as GT_zother_diff
+	, 'GT' as [Source]
+from hgmx_gt_dlocal_fulljoin_all
+where invoice_id is null
+group by isnull(gt_date, creation_date)
+),
+dlocal as (
+select 
+	isnull(creation_date, gt_date) as CREATION_DATE
+	,count(isnull(transaction_id, 'dlocal_null')) as DLOCAL_NULL_Count 
+	, sum(zother_diff) as DLOCAL_zother_diff
+	, 'GT' as [Source]
+from hgmx_gt_DLOCAL_fulljoin_all
+where TRANSACTION_ID is null
+group by ISNULL(CREATION_date, gt_date)
+),
+total as (
+select
+	isnull(gt_date, CREATION_date) as total_Date
+	, count(*) as Total_Count
+	, sum(zother_diff) as Both_zother_diff
+from hgmx_gt_DLOCAL_fulljoin_all
+group by isnull(gt_date, CREATION_date)
+),
+both as (
+select
+	gt_date as both_date
+	, count(*) as both_count
+from HGMX_GT_DLOCAL_FullJoin_ALL
+where invoice_id is not null and TRANSACTION_ID is not null
+group by GT_DATE
+),
+unmatched as (
+select
+	isnull(gt_date, creation_date) as unmatched_date
+	, count(*) as unmatched_count
+from HGMX_GT_DLOCAL_FullJoin_ALL
+where invoice_id is null or TRANSACTION_ID is null
+group by isnull(gt_date, creation_date) 
+)
+
+select
+	total.total_Date as Date
+	, gt.GT_NULL_Count as [Not in GT]
+	, DLOCAL.DLOCAL_NULL_Count [Not in DLOCAL]
+	, unmatched.unmatched_count as [Total Un-matched Txns]
+	, both.both_count as [Total Matched Txns]
+	, total.Total_Count [Total Txns]
+	, ((cast(gt.gt_null_count as int) + cast(dlocal.dlocal_null_count as int)) / cast(total.Total_Count as int)) AS [% of Total Txns Un-matched]
+from total
+full join gt on total.Total_Date = gt.gt_Date
+full join DLOCAL on total.Total_Date = DLOCAL.CREATION_Date
+full join both on total.total_Date = both.both_date
+full join unmatched on total.total_Date = unmatched.unmatched_date
+order by total.Total_Date 
+
+*/
